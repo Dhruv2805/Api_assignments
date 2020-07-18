83f46dd5a44386ba0e26afb350de52ee317ebf1e
Sushma <Sushma@192.168.0.107>
2020-07-19 01:10:39 +0530

    selenium mav1 project (1st assignment)

---
+++ Mav1/src/main/java/Mav1project/Mav1/App.java
@@ -0,0 +1,13 @@
+package Mav1project.Mav1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Mav1/src/test/java/Mav1project/Mav1/AppTest.java
@@ -0,0 +1,20 @@
+package Mav1project.Mav1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
