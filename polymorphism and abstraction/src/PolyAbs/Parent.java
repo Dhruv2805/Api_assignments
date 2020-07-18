edebabff87cec07c78963e39221f6db32cfb1ed8
Sushma <Sushma@192.168.0.107>
2020-07-19 00:52:41 +0530

    polymorphism and abstraction assignments

---
+++ polymorphism and abstraction/src/PolyAbs/Parent.java
@@ -0,0 +1,5 @@
+package PolyAbs;
+
+public class Parent {
+
+}
---
+++ polymorphism and abstraction/src/PolyAbs/Prclass.java
@@ -0,0 +1,12 @@
+package PolyAbs;
+
+public class Prclass
+{	
+	public void Master(int a, int b)
+	{
+		int c;
+		c=a+b;
+		System.out.println("the addition functioanality output is "+c);
+	}
+
+}
